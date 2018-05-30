aeac2775cc2fb3d40e664911c15b1b797a0e7e30
canermercancs <mercancaner@gmail.com>
2018-05-30 17:40:39 +0300

    vggnets added

--- deepnets/models/convmodel.py
+++ deepnets/models/convmodel.py
@@ -3,17 +3,33 @@ import torch.nn as nn
 class ConvnetModel(nn.Module):
     def __init__(self, num_classes, model_name, model):
         super().__init__()
-        if model_name.startswith('alex'):
-            self.model_name = 'alexnet'
+        self.model_name = model_name
+        if self.model_name.startswith('alex'):
             self.features   = model.features
             self.classifier = nn.Sequential(
                 nn.Dropout(p = 0.5),
-                nn.Linear(256*6*6, 4096), # 9216->4096
+                nn.Linear(256*6*6, 4096), # 9216 -> 4096
                 nn.ReLU(inplace = True),
                 nn.Dropout(p = 0.5),
                 nn.Linear(4096, 4096),
                 nn.ReLU(inplace = True),
                 nn.Linear(4096, num_classes))
+
+        elif self.model_name.startswith('vgg'):
+            self.features   = model.features
+            self.classifier = nn.Sequential(
+                nn.Linear(512*7*7, 4096), # 25088 -> 4096
+                nn.ReLU(inplace = True),
+                nn.Dropout(p = 0.5),
+                nn.Linear(4096, 4096),
+                nn.ReLU(inplace = True),
+                nn.Dropout(p = 0.5),
+                nn.Linear(4096, num_classes),
+            )
+
+        #elif self.model_name.startswith('resnet'):
+        #   kinda more complicated to do than alex and vgg nets.
+        #   will do later on.  
         else:
             raise NotImplementedError
         # freeze the feature extraction parameters.
@@ -25,3 +41,5 @@ class ConvnetModel(nn.Module):
         inp = inp.view(inp.size(0), -1) # flatten the convolutional output to (batch_size x 256*6*6)
         inp = self.classifier(inp)
         return inp
+
+
