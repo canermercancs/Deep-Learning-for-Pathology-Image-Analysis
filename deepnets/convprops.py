fdb0ee825f1ec0f97a06a971b399041b3e2466fc
canermercancs <mercancaner@gmail.com>
2018-05-30 22:13:45 +0300

    syntax cleaning

--- deepnets/convprops.py
+++ deepnets/convprops.py
@@ -2,16 +2,22 @@
 import torchvision
 
 
-MEAN_CONVNET = {'alexnet': [0.485, 0.456, 0.406]} # mean for alexnet images
-STD_CONVNET  = {'alexnet': [0.229, 0.224, 0.225]} # standard deviation for alexnet images
+MEAN_CONVNET = {
+	'alexnet'	: [0.485, 0.456, 0.406],
+	'vgg' 		: [0.485, 0.456, 0.406]
+	} # mean for alexnet images
+STD_CONVNET  = {
+	'alexnet'	: [0.229, 0.224, 0.225],
+	'vgg' 		: [0.229, 0.224, 0.225]
+	} # standard deviation for alexnet images
 
-VGG_TYPES = {
-	'vgg11' : torchvision.models.vgg11
-	'vgg13' : torchvision.models.vgg13
-	'vgg16' : torchvision.models.vgg16
-	'vgg19' : torchvision.models.vgg19
-	'vgg11bn' : torchvision.models.vgg11bn
-	'vgg13bn' : torchvision.models.vgg13bn
-	'vgg16bn' : torchvision.models.vgg16bn
-	'vgg19bn' : torchvision.models.vgg19bn
+VGG_MODELS = {
+	'vgg11' : torchvision.models.vgg11,
+	'vgg13' : torchvision.models.vgg13,
+	'vgg16' : torchvision.models.vgg16,
+	'vgg19' : torchvision.models.vgg19,
+	'vgg11_bn' : torchvision.models.vgg11_bn,
+	'vgg13_bn' : torchvision.models.vgg13_bn,
+	'vgg16_bn' : torchvision.models.vgg16_bn,
+	'vgg19_bn' : torchvision.models.vgg19_bn
 }
