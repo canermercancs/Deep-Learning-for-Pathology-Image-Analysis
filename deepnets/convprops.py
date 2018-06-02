866331be1ff0184898769584455a22e02870aab6
canermercancs <caner.mercan@cs.bilkent.edu.tr>
2018-06-02 21:01:12 +0300

    added feature extraction class and functionality for convnets

--- deepnets/convprops.py
+++ deepnets/convprops.py
@@ -1,23 +1,30 @@
-
+"""
+author: Caner Mercan
+"""
 import torchvision
 
-
+MODULE_NAMES = 'alexnet', 'vgg'
 MEAN_CONVNET = {
-	'alexnet'	: [0.485, 0.456, 0.406],
-	'vgg' 		: [0.485, 0.456, 0.406]
-	} # mean for alexnet images
+    MODULE_NAMES[0] : [0.485, 0.456, 0.406],
+    MODULE_NAMES[1] : [0.485, 0.456, 0.406]
+    } # mean for alexnet images
 STD_CONVNET  = {
-	'alexnet'	: [0.229, 0.224, 0.225],
-	'vgg' 		: [0.229, 0.224, 0.225]
-	} # standard deviation for alexnet images
-
+    MODULE_NAMES[0] : [0.229, 0.224, 0.225],
+    MODULE_NAMES[1] : [0.229, 0.224, 0.225]
+    } # standard deviation for alexnet images
+# efficient because doesn't load functions into memory; only their handles.
 VGG_MODELS = {
-	'vgg11' : torchvision.models.vgg11,
-	'vgg13' : torchvision.models.vgg13,
-	'vgg16' : torchvision.models.vgg16,
-	'vgg19' : torchvision.models.vgg19,
-	'vgg11_bn' : torchvision.models.vgg11_bn,
-	'vgg13_bn' : torchvision.models.vgg13_bn,
-	'vgg16_bn' : torchvision.models.vgg16_bn,
-	'vgg19_bn' : torchvision.models.vgg19_bn
+    'vgg11' : torchvision.models.vgg11,
+    'vgg13' : torchvision.models.vgg13,
+    'vgg16' : torchvision.models.vgg16,
+    'vgg19' : torchvision.models.vgg19,
+    'vgg11_bn' : torchvision.models.vgg11_bn,
+    'vgg13_bn' : torchvision.models.vgg13_bn,
+    'vgg16_bn' : torchvision.models.vgg16_bn,
+    'vgg19_bn' : torchvision.models.vgg19_bn
+}
+# number of 'classifier' modules to pop for feature extraction.
+FEATURE_POP_OPS = {
+    MODULE_NAMES[0] : 2,
+    MODULE_NAMES[1] : 3
 }
--- deepnets/models/convmodel.py
+++ deepnets/models/convmodel.py
@@ -1,10 +1,19 @@
+"""
+author: Caner Mercan
+"""
+
+import copy
 import torch.nn as nn
+from deepnets import convprops as CP
+
+__names__ = 'alexnet', 'vggnet' # currently implemented models
 
 class ConvnetModel(nn.Module):
-    def __init__(self, num_classes, model_name, model):
+    def __init__(self, num_classes, name, model):
         super().__init__()
-        self.model_name = model_name
-        if self.model_name.startswith('alex'):
+        
+        if name.startswith('alex'):
+            self.name = CP.MODULE_NAMES[0] # alexnet
             self.features   = model.features
             self.classifier = nn.Sequential(
                 nn.Dropout(p = 0.5),
@@ -13,8 +22,9 @@ class ConvnetModel(nn.Module):
                 nn.Dropout(p = 0.5),
                 nn.Linear(4096, 4096),
                 nn.ReLU(inplace = True),
-                nn.Linear(4096, num_classes))
-        elif self.model_name.startswith('vgg'):
+                nn.Linear(4096, num_classes))            
+        elif name.startswith('vgg'):
+            self.name = CP.MODULE_NAMES[1] # vgg
             self.features   = model.features
             self.classifier = nn.Sequential(
                 nn.Linear(512*7*7, 4096), # 25088 -> 4096
@@ -23,20 +33,55 @@ class ConvnetModel(nn.Module):
                 nn.Linear(4096, 4096),
                 nn.ReLU(inplace = True),
                 nn.Dropout(p = 0.5),
-                nn.Linear(4096, num_classes))
-        #elif self.model_name.startswith('resnet'):
+                nn.Linear(4096, num_classes))            
+        #elif self.name.startswith('resnet'):
         #   kinda more complicated to do than alex and vgg nets.
-        #   will do later on.  
+        #   will do later on.          
         else:
             raise NotImplementedError
+            
         # freeze the feature extraction parameters.
         for params in self.features.parameters():
             params.requires_grad = False
 
     def forward(self, inp):
         inp = self.features(inp)
-        inp = inp.view(inp.size(0), -1) # flatten the convolutional output to (batch_size x 256*6*6)
+        inp = inp.view(inp.size(0), -1) # flatten the convolutional output to (batch_size x #neurons)
         inp = self.classifier(inp)
         return inp
 
+    # def extract_features(self, inp):
+    #     #features        = copy.deepcopy(self.features)
+    #     classifier_list = list(copy.deepcopy(self.classifier))
 
+    #     # how many of the last modules of the net should be popped to get to the second from the last modules output.
+    #     num_pops = CP.FEATURE_POP_OPS[self.name]
+    #     for _ in range(num_pops):
+    #         classifier_list.pop()
+        
+    #     classifier = nn.Sequential(*classifier_list)
+    #     # now run the 
+    #     inp = self.features(inp)
+    #     inp = inp.view(inp.size(0), -1) # flatten the convolutional output to (batch_size x #neurons)
+    #     inp = classifier(inp)
+    #     return inp
+
+class ConvnetFeatures():
+    def __init__(self, model):
+        self.name       = model.name
+        self.features   = copy.deepcopy(model.features)
+        self.extractors = copy.deepcopy(model.classifier)
+        self.num_pops   = CP.FEATURE_POP_OPS[model.name]
+        self.__hook_fclayer__()
+
+    def __hook_fclayer__(self):
+        fclayer_list = list(self.extractors)
+        for _ in range(self.num_pops):
+            fclayer_list.pop()
+        self.extractors = nn.Sequential(*fclayer_list)
+
+    def __call__(self, inp):
+        inp = self.features(inp)
+        inp = np.view(inp.view(0), -1)
+        inp = self.extractors(inp)
+        return inp
